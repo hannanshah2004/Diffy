b732cdc9e403074cd24dbe084ac7e24e0e2c752a
Hannan Syed Shah <hannansshah2004@gmail.com>
2025-04-09 21:04:59 -0400

    changelog removed doesnt look nice

--- src/pages/PublicChangelog.tsx
+++ src/pages/PublicChangelog.tsx
@@ -138,12 +138,6 @@ export function PublicChangelog() {
 
   return (
     <div className="max-w-4xl mx-auto px-4 py-8">
-      <div>
-        <h1 className="text-3xl font-bold text-gray-900">Changelog</h1>
-        <p className="mt-2 text-gray-600">
-          Keep track of changes and upgrades to our platform.
-        </p>
-      </div>
 
       {/* Search and filters */}
       <div className="mt-8 mb-6">
